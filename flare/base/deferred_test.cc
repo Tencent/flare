cd0aae8feb23aa6375a0b09f0569da927ee7a4c5
Luo Bo <luobodi@hotmail.com>
2021-06-25 16:24:45 +0800

    Fixed move assignment of Deferred.

--- flare/base/deferred_test.cc
+++ flare/base/deferred_test.cc
@@ -40,6 +40,29 @@ TEST(Deferred, All) {
   }
   ASSERT_TRUE(f1);
   ASSERT_FALSE(f2);
+
+  bool f3 = false;
+  Deferred defer3([&] { f3 = true; });
+  ASSERT_FALSE(f3);
+  defer3.Fire();
+  ASSERT_TRUE(f3);
+
+  bool f4;
+  Deferred defer5;
+  {
+    Deferred defer4([&] { f4 = true; });
+    ASSERT_FALSE(f4);
+    defer5 = std::move(defer4);
+  }
+  ASSERT_FALSE(f4);
+  auto defer6 = std::move(defer5);
+  defer5.Fire();
+  ASSERT_FALSE(f4);
+  defer6.Fire();
+  ASSERT_TRUE(f4);
+
+  Deferred().Fire();     // Nothing happens.
+  Deferred().Dismiss();  // Nothing happens.
 }
 
 }  // namespace flare
